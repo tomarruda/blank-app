51c4c3f57b7d9d8c8fa8851ea9cbe96c272abb62
tomarruda <tarrudaw@gmail.com>
2025-02-09 17:36:59 +0000

    Segunda modificação

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 Meu primeiro aplicativo com Streamlit")
+st.title("Bem-vindo à qTED")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
