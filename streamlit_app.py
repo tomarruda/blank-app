2a3843269bcec46b2009ca6319540a63d56b21d5
tomarruda <tarrudaw@gmail.com>
2025-02-10 14:22:14 +0000

    Atualiza nome da Página Inicial na sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit as st
 
+st.set_page_config(page_title="Página Inicial", page_icon="🏠")
+st.sidebar.title("🏠 Página Inicial")
+
 st.title("Meu App Streamlit")
 st.write("Se você está vendo isso, o deploy funcionou!")
